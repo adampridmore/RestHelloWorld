5713056c4fd0f14156b065ee26a7af2d5e57e52b
Adam Pridmore <adam_pridmore@hotmail.com>
2014-10-03 10:17:07 +0100

    - Initial commit.

---
+++ src/main/java/com/masternaut/apachesandbox/TestWebService.java
@@ -0,0 +1,26 @@
+package com.masternaut.apachesandbox;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.ServletContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import java.util.Arrays;
+
+@Service
+@Path("/test")
+@Produces("application/json")
+public class TestWebService {
+    @GET
+    public String HelloWorld() {
+        return "HelloWorld";
+    }
+
+    @GET()
+    @Path("/helloWorld2")
+    public String HelloWorld2() {
+        return "HelloWorld";
+    }
+}
